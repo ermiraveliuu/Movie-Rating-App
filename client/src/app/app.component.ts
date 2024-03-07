5ea8d5ce7f38ff817cf6ef8093c5b3f09f2cfb67
ermiraveliuu <160069065+ermiraveliuu@users.noreply.github.com>
2024-03-07 22:44:16 +0100

    Remove placeholder component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,16 +1,24 @@
 import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
 import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER } from "@taiga-ui/core";
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Title} from "@angular/platform-browser";
+import {TuiAccordionModule} from "@taiga-ui/kit";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TuiRootModule, TuiDialogModule, TuiAlertModule],
+  imports: [RouterOutlet, TuiRootModule, TuiDialogModule, TuiAlertModule, TuiAccordionModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
     providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}]
 })
-export class AppComponent {
-  title = 'movie-rating-app';
+export class AppComponent implements OnInit {
+  private readonly title = inject(Title);
+
+  ngOnInit(): void {
+    this.title.setTitle('Reel Review')
+  }
+
+
 }
